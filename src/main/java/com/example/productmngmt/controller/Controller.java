197226595d88a0427b171adccce9856ef2de2f1a
Tanmay Shakya <tanmayshakya98@gmail.com>
2020-12-03 12:23:34 +0530

    no message

--- src/main/java/com/example/productmngmt/controller/Controller.java
+++ src/main/java/com/example/productmngmt/controller/Controller.java
@@ -23,60 +23,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.productmngmt.entity.Product;
 import com.example.productmngmt.exceptionhandler.ResponseMessage;
-import com.example.productmngmt.service.impl.ProductServiceImpl;
+import com.example.productmngmt.service.ProductService;
 
 @Validated
 @RestController
 public class Controller {
 
 	@Autowired
-	ProductServiceImpl proServiceImpl;
+	ProductService proService;
 
 	@PostMapping("create")
 	public ResponseEntity<?> createProduct(@Valid @RequestBody List<@Valid Product> products) {
-		return ResponseEntity.ok(proServiceImpl.create(products));
+		return ResponseEntity.ok(proService.create(products));
 	}
 
 	@GetMapping("getproduct/{pid}")
 	public ResponseEntity<?> getProduct(@PathVariable Long pid) {
-		return ResponseEntity.ok(proServiceImpl.getProduct(pid));
+		return ResponseEntity.ok(proService.getProduct(pid));
 	}
 
 	@PutMapping("update/{pid}")
 	public ResponseEntity<?> updateProduct(@PathVariable Long pid, @RequestBody Product product) {
-		return ResponseEntity.ok(proServiceImpl.updateProd(pid, product));
+		return ResponseEntity.ok(proService.updateProd(pid, product));
 	}
 
 	@GetMapping("getall")
 
 	public Page<Product> getall(Pageable pageable) {
-		return proServiceImpl.getAll(pageable);
+		return proService.getAll(pageable);
 	}
 
 	@GetMapping("getall/{search}")
 
 	public Page<Product> getall(@PathVariable String search, Pageable pageable) {
-		return proServiceImpl.getAll(search, pageable);
+		return proService.getAll(search, pageable);
 	}
 
 	@DeleteMapping("delete/{pid}")
 	public ResponseEntity<?> deleteProduct(@PathVariable Long pid) {
 		return new ResponseEntity<ResponseMessage>(
-				new ResponseMessage(new Date(), HttpStatus.OK, "Product with Id : " + proServiceImpl.deleteProd(pid) + " is deleted"),
+				new ResponseMessage(new Date(), HttpStatus.OK, "Product with Id : " + proService.deleteProd(pid) + " is deleted"),
 				new HttpHeaders(), HttpStatus.OK);
 	}
 
 	@PostMapping("addStock")
 	public ResponseEntity<?> addStock(@RequestBody Map<Long, Long> stockList) {
 		return new ResponseEntity<ResponseMessage>(
-				new ResponseMessage(new Date(), HttpStatus.OK, proServiceImpl.addStock(stockList)),
+				new ResponseMessage(new Date(), HttpStatus.OK, proService.addStock(stockList)),
 				new HttpHeaders(), HttpStatus.OK);
 	}
 
 	@PostMapping("removeStock")
 	public ResponseEntity<?> removeStock(@RequestBody Map<Long, Long> stockList) {
 		return new ResponseEntity<ResponseMessage>(
-				new ResponseMessage(new Date(), HttpStatus.OK, proServiceImpl.removeStock(stockList)),
+				new ResponseMessage(new Date(), HttpStatus.OK, proService.removeStock(stockList)),
 				new HttpHeaders(), HttpStatus.OK);
 	}
 }
